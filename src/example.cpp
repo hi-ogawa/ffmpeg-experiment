c233974704dcb34313065dbe713966a93e27b449
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-10-21 22:22:44 +0900

    feat: add custom_log_callback

--- src/example.cpp
+++ src/example.cpp
@@ -22,13 +22,13 @@ struct Example {
   ~Example() { avformat_close_input(&fmt_ctx_); }
 };
 
-constexpr size_t AVIO_BUFFER_SIZE = 1 << 12;
-
 struct Buffer {
   AVIOContext *avio_ctx_;
   void *avio_buffer_;         // ffmpeg internal buffer
   std::vector<uint8_t> data_; // actual data
   std::vector<uint8_t>::iterator data_iter_;
+
+  static constexpr size_t AVIO_BUFFER_SIZE = 1 << 12;
 
   Buffer(const std::vector<uint8_t> &data) : data_{data} {
     data_iter_ = data_.begin();
@@ -61,11 +61,33 @@ struct Buffer {
   }
 };
 
-// TODO: customize callback
-// void custom_log_callback(void*, int, const char*, va_list) {}
-// av_log_set_callback(custom_log_callback);
+std::vector<std::tuple<int, std::string>> g_log;
+
+void custom_log_callback(void *, int level, const char *fmt, va_list vl) {
+  // cf. https://stackoverflow.com/a/49812356
+
+  // "dry run" to compute the length
+  va_list vl_copy;
+  va_copy(vl_copy, vl);
+  auto len = std::vsnprintf(NULL, 0, fmt, vl_copy);
+  va_end(vl_copy);
+
+  // print to buffer
+  ASSERT(len >= 0);
+  std::vector<char> buffer(len + 1);
+  std::vsnprintf(buffer.data(), buffer.size(), fmt, vl);
+  va_end(vl);
+
+  // push log
+  std::string log{buffer.data(), buffer.size()};
+  g_log.push_back(std::make_tuple(level, log));
+
+  std::cout << log << std::flush;
+}
 
 int main(int argc, const char **argv) {
+  av_log_set_callback(custom_log_callback);
+
   utils::Cli cli{argc, argv};
   auto infile = cli.argument<std::string>("-i").value_or("test.webm");
 
@@ -77,6 +99,8 @@ int main(int argc, const char **argv) {
   ASSERT(avformat_open_input(&example.fmt_ctx_, NULL, NULL, NULL) == 0);
   ASSERT(avformat_find_stream_info(example.fmt_ctx_, NULL) == 0);
   av_dump_format(example.fmt_ctx_, 0, NULL, 0);
+
+  dbg(g_log);
 
   return 0;
 }
