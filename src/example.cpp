e363966d8dba992967c1aa3b244113e429895cdc
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-10-21 13:27:04 +0900

    wip: reading in-memory buffer for avformat

--- src/example.cpp
+++ src/example.cpp
@@ -1,7 +1,15 @@
 #include "utils.hpp"
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+// based on third_party/FFmpeg/doc/examples/avio_reading.c
 
 extern "C" {
   #include <libavformat/avformat.h>
+  #include <libavformat/avio.h>
+  #include <libavutil/avutil.h>
 }
 
 struct Example {
@@ -14,10 +22,58 @@ struct Example {
 
   ~Example() {
     avformat_close_input(&fmt_ctx_);
+    // avformat_free_context(fmt_ctx_);
   }
 };
 
+struct Buffer {
+  AVIOContext* avio_ctx_;
+  std::vector<uint8_t> data_;
+
+  Buffer(const std::vector<uint8_t>& data): data_{data} {
+    avio_ctx_ = avio_alloc_context(data_.data(), data_.size(), 0, this, NULL, NULL, NULL);
+    ASSERT(avio_ctx_);
+  }
+
+  ~Buffer() {
+    avio_context_free(&avio_ctx_);
+  }
+};
+
+// TODO: customize callback
+// void custom_log_callback(void*, int, const char*, va_list) {
+// }
+// av_log_set_callback(custom_log_callback);
+
+std::vector<uint8_t> read_file(const std::string& filename) {
+  std::ifstream istr(filename, std::ios::binary);
+  ASSERT(istr.is_open());
+  std::vector<uint8_t> data((std::istreambuf_iterator<char>(istr)), std::istreambuf_iterator<char>());
+  return data;
+}
+
 int main() {
   Example example;
+
+  // yt-dlp -f 251 -o test.webm https://www.youtube.com/watch?v=uWEcvd7wk3U
+  auto data = read_file("test.webm");
+  Buffer buffer(data);
+
+  example.fmt_ctx_->pb = buffer.avio_ctx_;
+  ASSERT(avformat_open_input(&example.fmt_ctx_, NULL, NULL, NULL) == 0);
+  ASSERT(avformat_find_stream_info(example.fmt_ctx_, NULL) == 0);
+  av_dump_format(example.fmt_ctx_, 0, NULL, 0);
+
+  // TODO: segfault on cleanup
+  // $ cmake --build build/cc/Debug -- example-local
+  // [2/2] Linking CXX executable example-local
+  // $ ./build/cc/Debug/example-local
+  // Input #0, matroska,webm, from '(null)':
+  //   Metadata:
+  //     encoder         : google/video-file
+  //   Duration: 00:04:12.40, start: -0.007000, bitrate: N/A
+  //   Stream #0:0(eng): Audio: opus, 48000 Hz, 2 channels (default)
+  // Erreur de segmentation (core dumped)
+
   return 0;
 }
--- src/utils.hpp
+++ src/utils.hpp
@@ -1,5 +1,6 @@
 #include <stdexcept>
 #include <sstream>
+#include <tuple>
 
 #define ASSERT(EXPR)                                                 \
   if(!static_cast<bool>(EXPR)) {                                    \
@@ -7,3 +8,14 @@
     ostream << "[" << __FILE__ << ":" << __LINE__ << "] " << #EXPR; \
     throw std::runtime_error{ostream.str()};                        \
   }
+
+
+// quick pretty print
+namespace std {
+  template<class ...Ts>        ostream& operator<<(ostream& o, const tuple<Ts...>& x) { o << "("; bool b = 0; apply([&](auto&&... y){ ((o << (b ? ", " : "") << y, b = 1), ...); }, x); return o << ")"; }
+  template<class T1, class T2> ostream& operator<<(ostream& o, const pair<T1, T2>& x) { return o << tie(x.first, x.second); }
+  template<class T, class = decltype(begin(declval<T>())), class = enable_if_t<!is_same<T, string>::value>>
+  ostream& operator<<(ostream& o, const T& x) { o << "{"; bool b = 0; for (auto& y : x) { o << (b ? ", " : "") << y; b = 1; } return o << "}"; }
+}
+
+#define dbg(...) do { std::cout << #__VA_ARGS__ ": " << std::make_tuple(__VA_ARGS__) << std::endl; } while (0)
